e1c5f0c5277e33211cc39d4a2c4ae82534412bcc
Allan Sifuna <35229350+allansifuna@users.noreply.github.com>
2018-12-17 23:01:05 +0300

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,8 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USERNAME'] = 'noreply.sifuna@gmail.com'
-# app.config['MAIL_PASSWORD'] = 'namasaka88'
 app.config['MAIL_USERNAME'] = os.environ.get('email')
 app.config['MAIL_PASSWORD'] = os.environ.get('Password')
 
